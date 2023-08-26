ac55114b2ba4902500a2da35aedcb2d789a7fa93
Amirhossein <amirmsh@student.ubc.ca>
2023-08-26 11:29:34 -0700

    added sample codes for learning steps

--- codes/test.py
+++ codes/test.py
@@ -1,19 +1,6 @@
-# from langchain.llms import OpenAI
+from langchain.llms import OpenAI
 
-# llm = OpenAI(model="text-davinci-003", temperature=0.9)
+llm = OpenAI(model="text-davinci-003", temperature=0.9)
 
-# text = "Suggest a personalized workout routine for someone looking to improve cardiovascular endurance and prefers outdoor activities."
-# print(llm(text))
-
-
-import openai
-
-try:
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt="Hello, world!",
-        max_tokens=5
-    )
-    print("API Key is valid.")
-except openai.error.OpenAIError:
-    print("API Key is not valid.")
+text = "Suggest a personalized workout routine for someone looking to improve cardiovascular endurance and prefers outdoor activities."
+print(llm(text))
