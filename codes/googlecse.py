d1fb966dfde9345d66bc5b44e1dd18f04d5c9dc1
Amirhossein <amirmsh@student.ubc.ca>
2023-08-28 18:29:13 -0700

    Added codes - Zero to Hero langchain

---
+++ codes/googlecse.py
@@ -0,0 +1,12 @@
+from langchain.tools import Tool
+from langchain.utilities import GoogleSearchAPIWrapper
+
+search = GoogleSearchAPIWrapper()
+
+tool = Tool(
+    name="Google Search",
+    description="Search Google for recent results.",
+    func=search.run,
+)
+
+tool.run("Obama's first name?")
